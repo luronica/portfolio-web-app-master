0aac33f2b97a520a6d598f855487ca5d39a78a0d
Varnika Kambley <varnikakambley@gmail.com>
2025-10-10 16:40:00 +0530

    git push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,8 +74,8 @@ export default function Home() {
 					<div className="md:w-1/2 relative">
 						<div className="relative flex justify-center py-4 md:py-8">
 							<img
-								src="https://picsum.photos/200/200"
-								alt="Profile"
+								src="/images/profile.jpg"
+								alt="Varnika Kambley"
 								className="mx-auto rounded-full border-4 border-teal-400 shadow-lg object-cover w-44 h-44 sm:w-48 sm:h-48 md:w-56 md:h-56"
 							/>
 						</div>
